50efd855836e8aaec44182f027e81438ccfeb667
wilpat80 <wilmer_andrey@hotmail.com>
2021-07-19 21:49:40 -0500

    ejerciciopoo2 190721 2149

--- app/src/main/java/com/example/banco/MainActivity.java
+++ app/src/main/java/com/example/banco/MainActivity.java
@@ -4,23 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtresultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        txtresultado = findViewById(R.id.textView);
 
         }
 
 public void mostrar1 (View view) {
     Cuenta usuario1 = new Cuenta();
-    Toast.makeText(this, "Prueba1", Toast.LENGTH_SHORT).show();
-    Toast.makeText(this, "Titular" + usuario1.titular, Toast.LENGTH_SHORT).show();
+    txtresultado.setText("Titular "+ usuario1.titular+" saldo: "+usuario1.cantidad);
+  //  Toast.makeText(this, "Titular" + usuario1.titular+" y el saldo es "+usuario1.cantidad, Toast.LENGTH_LONG).show();
+    //Toast.makeText(this, "El saldo es: "+ usuario1.cantidad, Toast.LENGTH_LONG).show();
+
 }
 
 }
